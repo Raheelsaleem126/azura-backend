c2ab4d50726f70b2c8b536d225313008e2b85e58
Muhammad Maaz Nizam <94466009+maaznizam290@users.noreply.github.com>
2023-05-18 00:35:27 +0500

    refactor the estimated document count function

--- server.js
+++ server.js
@@ -1,78 +1,49 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./models");
+const { PORT } = require("./config/db.config");
 const app = express();
+const Role = db.role;
 
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect(`mongodb+srv://root:admin@cluster0.zkcbtdo.mongodb.net/`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Successfully connect to MongoDB.");
-    initial();
   })
   .catch((err) => {
     console.error("Connection error", err);
     process.exit();
   });
-
 function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
+  return Role.estimatedDocumentCount()
+    .then((count) => {
+      if (count === 0) {
+        return Promise.all([
+          new Role({ name: "user" }).save(),
+          new Role({ name: "moderator" }).save(),
+          new Role({ name: "admin" }).save(),
+        ]);
+      }
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
 
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
+initial()
+  .then((savedRoles) => {
+    if (savedRoles) {
+      savedRoles.forEach((savedRole) => {
+        console.log(`Added '${savedRole.name}' to roles collection`);
       });
     }
+  })
+  .catch((error) => {
+    console.log("Error:", error);
   });
-}
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-// routes
-require("./app/routes/auth.routes")(app);
-require("./app/routes/user.routes")(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`The server is running on the https://localhost:${PORT}`);
 });
